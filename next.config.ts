ede1f9edc6b86ed13400b6e84cf19ece5525728b
KimEunHye <gpqjr00@naver.com>
2026-04-09 21:45:18 +0900

    Fix: turbopack 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+   turbopack: {    
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
