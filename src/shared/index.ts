574da16ed50e8728b5850503dfebd4a8bbd02ac3
KimEunHye <gpqjr00@naver.com>
2026-04-16 22:00:27 +0900

    Feat: svg 아이콘 export

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -6,3 +6,11 @@ export { WeatherIcon } from './ui/WeatherIcon';
 
 // ./types
 export type { WeatherIconType } from './model/types';
+
+// ./asset
+export {default as SearchIcon} from '@/shared/asset/search.svg';
+export {default as LocationIcon} from '@/shared/asset/location.svg';
+export {default as CloudIcon} from '@/shared/asset/cloud.svg';
+export {default as StarIcon} from '@/shared/asset/start.svg';
+export {default as StarColorIcon} from '@/shared/asset/star_color.svg';
+export {default as PencilIcon} from '@/shared/asset/pencil.svg';
