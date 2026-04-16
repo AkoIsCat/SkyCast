826d30e1aad34484993d67d8f9f401e86949ae73
KimEunHye <gpqjr00@naver.com>
2026-04-16 21:59:37 +0900

    Fix: svgr 패키지 적용 완료

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-   turbopack: {    
+   turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
     return config;
   },
 };
