1862a7376bc47055df29db79498c24a63c3f2c34
KimEunHye <gpqjr00@naver.com>
2026-04-23 20:11:34 +0900

    Design: 구름 아이콘 크기 변경

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -3,8 +3,8 @@ import CloudIcon from '@/shared/asset/cloud.svg';
 export const Logo = () => {
   return (
     <div className="flex items-center gap-3">
-      <CloudIcon />
-      <p className="text-2xl font-semibold lg:text-4xl">Weather</p>
+      <CloudIcon className="w-14 h-14 lg:w-12 lg:h-12" />
+      <p className="text-2xl font-medium lg:text-4xl">Weather</p>
     </div>
   );
 };
